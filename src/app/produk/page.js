3c80b1e04d7c924ef1d7200f86c23ee59ef2b2b7
DylanAvillaSa <dylanavilla790@gmail.com>
2025-09-26 11:04:15 +0700

    ip

--- src/app/produk/page.js
+++ src/app/produk/page.js
@@ -106,7 +106,7 @@ export default function CameraPage() {
         {ip ? (
           <div className="border rounded-xl overflow-hidden shadow-md mb-4">
             <img
-              src={`https://polyspermia-breann-parallelly.ngrok-free.dev/stream?ngrok-skip-browser-warning=true`}
+              src={`http://${ip}/stream`}
               alt="ESP32 CAM Stream"
               className="w-full object-cover aspect-video"
             />
